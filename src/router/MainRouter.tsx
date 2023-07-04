2297eef7b5eea3b7ae5756bd2264a96d1abfc73c
kimjuwon <juwon2298@naver.com>
2023-07-05 08:50:28 +0900

    react router 수정

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/Login/LoginPage";
 import SignUpPage from "../pages/Signup/SignUpPage";
+import DetailSignUpPage from "../pages/Signup/DetailSignUpPage";
 
 function MainRouter() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/2signup" element={<DetailSignUpPage />} />
     </Routes>
   );
 }
