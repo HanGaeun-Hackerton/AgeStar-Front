26330fdaa9a2601072d5fc9cc328e36f012eddce
kimjuwon <juwon2298@naver.com>
2023-07-05 08:52:10 +0900

    데이터 수정

--- src/components/constants/index.ts
+++ src/components/constants/index.ts
@@ -1,6 +1,7 @@
 import { LoginInputDataType } from "../../model/Login";
+import { SignUpInputDataType } from "../../model/Signup";
 
-export const LoginInpuData: LoginInputDataType[] = [
+export const LoginInputData: LoginInputDataType[] = [
   {
     title: "아이디",
     name: "id",
@@ -8,5 +9,35 @@ export const LoginInpuData: LoginInputDataType[] = [
   {
     title: "비밀번호",
     name: "password",
+  },
+];
+
+export const SignUpInputData: SignUpInputDataType[] = [
+  {
+    title: "이름",
+    name: "name",
+  },
+  {
+    title: "아이디",
+    name: "id",
+  },
+  {
+    title: "비밀번호",
+    name: "password",
+  },
+];
+
+export const DetailSignUpInputData: SignUpInputDataType[] = [
+  {
+    title: "출생연도",
+    name: "birthyear",
+  },
+  {
+    title: "주민등록번호",
+    name: "citynumber",
+  },
+  {
+    title: "전화번호",
+    name: "phonenumber",
   },
 ];
